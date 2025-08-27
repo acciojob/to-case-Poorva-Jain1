a8135f813957db3aa96a3aa0c36087e7f008f039
Poorva-Jain1 <poorvajainnagod@gmail.com>
2025-08-27 23:29:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 function toCase(text) {
   // write your code here
+	if (text.length === 0) {
+    return "-";
+  }
+	return text.toLowerCase() + "-" + text.toUpperCase();
 }
+
 
 // DO not change the code below
 
